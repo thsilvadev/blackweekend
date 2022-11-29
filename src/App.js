f910c4647232464b37abb47e7bb5497b6d7824f6
Thiago Silva <thsilva.developer@gmail.com>
2022-11-29 00:03:54 -0300

    Primeiro upload, apenas um React puro, sem nada.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Black Weekend</h1>
+      <h2>Cinema de qualidade</h2>
+    </div>
+  );
+}
+
+export default App;
